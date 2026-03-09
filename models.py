85f3e7e4c0ed5f9b23ed004e684e514d16d54530
Perry655 <97612695+Perry655@users.noreply.github.com>
2026-03-09 19:26:59 +0800

    Update models.py

--- models.py
+++ models.py
@@ -51,9 +51,9 @@ def model_parser(model_name, parser, add_help=False):
     if model_name == 'WaveGlow':
         from waveglow.arg_parser import waveglow_parser
         return waveglow_parser(parser, add_help)
-    if model_name == 'HiFi-Gan':
+    if model_name == 'HiFi-GAN':
         from hifigan.arg_parser import hifigan_parser
-        return hifigan_parser(parser, add_help)
+        return parse_hifigan_args(parser, add_help)
     else:
         raise NotImplementedError(model_name)
 
